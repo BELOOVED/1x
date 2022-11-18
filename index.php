c64627742158c104699f8d53c7f632139dbaebb6
N1C3B00T <77572556+N1C3B00T@users.noreply.github.com>
2022-11-18 22:21:27 +0300

    Update index.php

--- index.php
+++ index.php
@@ -1,13 +1,10 @@
 <?php
   include 'config.php';
-  if (empty(intval($_SESSION['user_id']))){
-    $page = "login";
-  }else{
-        $q = explode("/", $_GET['q']);
-        $page = "main";
-        if (file_exists('view/'.$q[0].'.php')) {
-            $page = $q[0];
-        }
+
+  $q = explode("/", $_GET['q']);
+  $page = "main";
+  if (file_exists('view/'.$q[0].'.php')) {
+      $page = $q[0];
   }
 
 //   include 'view/modules/header.php';
